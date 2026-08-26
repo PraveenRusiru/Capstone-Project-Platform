187971ef56721004f60473a56854f7334d5e6191
praveenrusiru <praveenrusiru752@gmail.com>
2026-08-26 17:42:23 +0530

    ecosystem changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,25 @@
-
-
 module.exports = {
-    apps : [{
-        name   : "Config-server",
-        script : "java -jar ./Config-server/target/Config-Server-1.0.0.jar",
-        log_files:"./log/config-server.log"
-    },{
-        name   : "Service-registry",
-        script : "java -jar  ./Service-registry/target/Service-Registry-1.0.0.jar",
-        log_files:"./log/service-registry.log"
-    },{
-        name   : "API-gateway",
-        script : "java -jar ./API-gateway/target/Api-Gateway-1.0.0.jar",
-        log_files:"./log/api-gateway.log"
-    }]
-}
-
+    apps: [
+        {
+            name: "Config-server",
+            script: "java",
+            args: "-jar ./Config-server/target/Config-Server-1.0.0.jar",
+            error_file: "./log/config-server-error.log",
+            out_file: "./log/config-server-out.log"
+        },
+        {
+            name: "Service-registry",
+            script: "java",
+            args: "-jar ./Service-registry/target/Service-Registry-1.0.0.jar",
+            error_file: "./log/service-registry-error.log",
+            out_file: "./log/service-registry-out.log"
+        },
+        {
+            name: "API-gateway",
+            script: "java",
+            args: "-jar ./API-gateway/target/Api-Gateway-1.0.0.jar",
+            error_file: "./log/api-gateway-error.log",
+            out_file: "./log/api-gateway-out.log"
+        }
+    ]
+};
