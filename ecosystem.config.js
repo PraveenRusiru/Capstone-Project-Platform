dc337b9ab1a61dafe133c97902d0cdc8a53dbdce
praveenrusiru <praveenrusiru752@gmail.com>
2026-08-26 17:50:45 +0530

    ecosystem changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,22 +2,19 @@ module.exports = {
     apps: [
         {
             name: "Config-server",
-            script: "java",
-            args: "-jar ./Config-server/target/Config-Server-1.0.0.jar",
+            script: "java -jar ./Config-server/target/Config-Server-1.0.0.jar",
             error_file: "./log/config-server-error.log",
             out_file: "./log/config-server-out.log"
         },
         {
             name: "Service-registry",
-            script: "java",
-            args: "-jar ./Service-registry/target/Service-Registry-1.0.0.jar",
+            script: "java jar ./Service-registry/target/Service-Registry-1.0.0.jar",
             error_file: "./log/service-registry-error.log",
             out_file: "./log/service-registry-out.log"
         },
         {
             name: "API-gateway",
-            script: "java",
-            args: "-jar ./API-gateway/target/Api-Gateway-1.0.0.jar",
+            script: "java -jar ./API-gateway/target/Api-Gateway-1.0.0.jar",
             error_file: "./log/api-gateway-error.log",
             out_file: "./log/api-gateway-out.log"
         }
