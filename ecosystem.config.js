951d042766e9127371246dcf9ce66ccb4fe42d6e
praveenrusiru <praveenrusiru752@gmail.com>
2026-08-27 10:17:35 +0530

    applications name changed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,21 +2,30 @@ module.exports = {
     apps: [
         {
             name: "Config-server",
-            script: "java -jar ./Config-server/target/Config-Server-1.0.0.jar",
-            error_file: "./log/config-server-error.log",
-            out_file: "./log/config-server-out.log"
+            script: "java",
+            args: "-jar -Dspring.profiles.active=native ./Config-server/target/Config-Server-1.0.0.jar",
+            interpreter: "none",
+            out_file: "./log/config-server-out.log",
+            error_file: "./log/config-server-err.log",
+            merge_logs: true
         },
         {
             name: "Service-registry",
-            script: "java jar ./Service-registry/target/Service-Registry-1.0.0.jar",
-            error_file: "./log/service-registry-error.log",
-            out_file: "./log/service-registry-out.log"
+            script: "java",
+            args: "-jar ./Service-registry/target/Service-Registry-1.0.0.jar",
+            interpreter: "none",
+            out_file: "./log/service-registry-out.log",
+            error_file: "./log/service-registry-err.log",
+            merge_logs: true
         },
         {
             name: "API-gateway",
-            script: "java -jar ./API-gateway/target/Api-Gateway-1.0.0.jar",
-            error_file: "./log/api-gateway-error.log",
-            out_file: "./log/api-gateway-out.log"
+            script: "java",
+            args: "-jar ./API-gateway/target/Api-Gateway-1.0.0.jar",
+            interpreter: "none",
+            out_file: "./log/api-gateway-out.log",
+            error_file: "./log/api-gateway-err.log",
+            merge_logs: true
         }
     ]
 };
